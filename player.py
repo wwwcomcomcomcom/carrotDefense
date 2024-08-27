baf2a80346e04c793435295a7a9393ee4d07215b
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-08-27 15:17:25 +0900

    player wall collision

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import pygame
 import json
 import time
 from imageLoader import loadImage, cutImageByUv
+from tile.tile import TileMap
 
 time.time()
 
@@ -13,13 +14,13 @@ class Player:
     state: str
 
     sprite: pygame.Surface
-
     animation: dict
-    # time.time()
     animatedTime: float
     animationFrame: int
 
-    def __init__(self):
+    world: TileMap
+
+    def __init__(self, world: TileMap):
         jsonData = None
         with open("playerData.json", "r") as file:
             jsonData = json.load(file)
@@ -29,6 +30,7 @@ class Player:
         self.animation = jsonData["animation"]
         self.animatedTime = time.time()
         self.animationFrame = 0
+        self.world = world
 
         loadImage(jsonData["sprite"])
 
@@ -54,17 +56,14 @@ class Player:
         if keys[pygame.K_DOWN]:
             player_movement.y = self.speed
 
+        tileX = (self.x + player_movement.x) / 64
+        tileY = (self.y + player_movement.y) / 64
+        if self.world.tiles[round(tileY)][round(tileX)].tile == "water":
+            print(tileX, tileY)
+            return
+
         self.x += player_movement.x
         self.y += player_movement.y
-
-    def getCameraTileRange(self):
-        # 1920/2 = 960 1080/2 = 540
-        return {
-            "start_x": int((self.x + 960) // 60 + 1),
-            "end_x": int((self.y - 960) // 60 - 1),
-            "start_y": int((self.y + 540) // 60 + 1),
-            "end_y": int((self.y - 540) // 60 - 1),
-        }
 
     def getCurrentSprite(self):
         if len(self.animation[self.state]["frames"]) == 1:
