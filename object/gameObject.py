520dc75300e550bebf98d735961f8abf95f5b32a
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-03 20:18:18 +0900

    game object class, Animation controlelr

--- object/gameObject.py
+++ object/gameObject.py
@@ -1,36 +1,64 @@
 import pygame
-import json
+from globalModule import windowHalfWidth, windowHalfHeight
+from typing import Dict, List, Tuple
+from imageLoader import cutImageByUv
+
+
+class Animation:
+    frames: List[pygame.Surface]
+    # Bigger is slower
+    speed: int
+    animatedTime: int
+    animationFrame: int
+
+    # "frames": [
+    #     {
+    #       "uv": [0, 0],
+    #       "size": [16, 16]
+    #     }
+    #   ]
+    def __init__(self, data: dict, sprite: pygame.Surface):
+        self.speed = data["speed"]
+        self.animatedTime = pygame.time.get_ticks()
+        self.animationFrame = 0
+        for uvData in data["frames"]:
+            uv = uvData["uv"]
+            size = uvData["size"]
+            self.frames.append(cutImageByUv(sprite, uv, size))
+
+    # also update frames
+    def getFrame(self) -> pygame.Surface:
+        if pygame.time.get_ticks() - self.animatedTime > self.speed:
+            self.animatedTime = pygame.time.get_ticks()
+            self.animationFrame += 1
+            if self.animationFrame >= len(self.frames):
+                self.animationFrame = 0
+        return self.frames[self.animationFrame]
 
 
 class GameObject:
     x: float
     y: float
     sprite: pygame.Surface
-    state: str
-    animation: dict
-    animatedTime: int
-    animationFrame: int
 
-    def __init__(self, x, y, sprite, state, animation, animatedTime, animationFrame):
+    def __init__(self, x, y, sprite):
         self.x = x
         self.y = y
         self.sprite = sprite
-        self.state = state
-        self.animation = animation
-        self.animatedTime = animatedTime
-        self.animationFrame = animationFrame
 
-    @classmethod
-    def from_json(filePath: str):
-        data = None
-        with open(filePath, "r") as file:
-            data = json.load(file)
-        return Object(
-            float(data["x"]),
-            float(data["y"]),
-            pygame.image.load(data["sprite"]),
-            "",
-            data["animation"],
-            pygame.time.get_ticks(),
-            0,
-        )
+    def getVecotor(self):
+        return pygame.math.Vector2(self.x, self.y)
+
+    def setPostion(self, x, y):
+        self.x = x
+        self.y = y
+
+    def setPositionWithVector(self, vector):
+        self.x = vector.x
+        self.y = vector.y
+
+    def getTexture(self):
+        return self.sprite
+
+    def render(self, screen: pygame.Surface):
+        screen.blit(self.sprite, (self.x, self.y))
