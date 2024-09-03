61bce5b518d428317001ebeb4bae8d97c97df923
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-03 21:18:54 +0900

    inherit GameObject, animate with Animation class

--- player.py
+++ player.py
@@ -2,51 +2,47 @@ import pygame
 import json
 import time
 from imageLoader import loadImage, cutImageByUv
-from tile.tile import TownTileMap
+from tile.tile import TileMap
+from object.gameObject import GameObject
+from object.animation import Animation
 
 
-class Player:
+class Player(GameObject):
     x: float
     y: float
+
+    animationState: str
+    animations: dict[str, Animation]
+
     speed: float
     size: float
-    state: str
+    world: TileMap
 
-    sprite: pygame.Surface
-    animation: dict
-    animatedTime: int
-    animationFrame: int
-
-    world: TownTileMap
-
-    def __init__(self, world: TownTileMap):
+    def __init__(self, world: TileMap):
         playerConfig = None
         with open("playerConfig.json", "r") as file:
             playerConfig = json.load(file)
+
         playerData = None
         with open("save/player.json", "r") as file:
             playerData = json.load(file)
+
         self.x = playerData["x"]
         self.y = playerData["y"]
-
         self.speed = playerConfig["speed"]
         self.size = float(playerConfig["size"])
-        self.sprite = pygame.image.load(playerConfig["sprite"]["imagePath"])
-        self.animation = playerConfig["animation"]
-        self.animatedTime = pygame.time.get_ticks()
-        self.animationFrame = 0
         self.world = world
 
-        loadImage(playerConfig["sprite"])
-        self.state = "idle"
+        self.animationState = "idle"
+        self.animations = Animation.listFromJson(
+            playerConfig["animations"], pygame.image.load(playerConfig["sprite"])
+        )
 
     def vector(self):
         return pygame.math.Vector2(self.x, self.y)
 
     def move(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_ESCAPE]:
-            running = False
         player_movement = pygame.math.Vector2(0, 0)
         if keys[pygame.K_LEFT]:
             player_movement.x = -self.speed
@@ -67,22 +63,7 @@ class Player:
         self.y += player_movement.y
 
     def getCurrentSprite(self):
-        if len(self.animation[self.state]["frames"]) == 1:
-            uv = self.animation[self.state]["frames"][0]["uv"]
-            size = self.animation[self.state]["frames"][0]["size"]
-            return cutImageByUv(self.sprite, uv, size, self.size * 4)
-        if (
-            time.time() - self.animatedTime
-            > self.animation[self.state][self.animationFrame]["time"]
-        ):
-            self.animationFrame += 1
-            self.animatedTime = time.time()
-
-        return cutImageByUv(
-            self.sprite,
-            self.animation[self.state][self.animationFrame]["uv"],
-            self.animation[self.state][self.animationFrame]["size"],
-        )
+        return self.animations[self.animationState].getFrame()
 
     def render(self, screen: pygame.Surface):
         screen.blit(self.getCurrentSprite(), [960 - 32, 540 - 32])
