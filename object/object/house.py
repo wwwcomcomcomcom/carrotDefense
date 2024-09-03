63f0e4f2e4c048d59c0beaebe817b0f95fa84d45
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-03 23:32:44 +0900

    house object

---
+++ object/object/house.py
@@ -0,0 +1,19 @@
+import pygame
+from object.gameObject import GameObject
+from globalModule import windowHalfHeight, windowHalfWidth
+from imageLoader import loadImageByData
+
+
+class House1(GameObject):
+
+    def __init__(self):
+        image = loadImageByData(
+            "assets\\map\\Buildings\\Purple\\PurpleChapels.png", (0, 16), (16, 16), 8
+        )
+        super().__init__(50 * 64, 50 * 64, image)
+
+    def render(self, screen: pygame.Surface, x, y):
+        screen.blit(
+            self.sprite,
+            (-x - 32 + windowHalfWidth + self.x, -y - 32 + windowHalfHeight + self.y),
+        )
