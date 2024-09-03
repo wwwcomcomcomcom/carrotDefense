aab87cbae364e8311f2077f4c061e4a723080b51
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-03 22:25:17 +0900

    fromJson

--- tile/tile.py
+++ tile/tile.py
@@ -2,34 +2,54 @@ import json
 import pygame
 from .pallete import Pallete
 from typing import List
+from globalModule import windowHalfHeight, windowHalfWidth
 
 
 class Tile:
+    # Readonly
     x: int
     y: int
     tile: str
+    state: dict
 
-    def __init__(self, x: int, y: int, tile: str):
+    def __init__(self, x: int, y: int, tile: str, state: dict = {}):
         self.x = x
         self.y = y
         self.tile = tile
 
+    @classmethod
+    def fromJson(cls, x: int, y: int, data: dict):
+        return cls(x, y, data["tile"], data["state"])
+
     def getTexture(self, pallete: Pallete):
         return pallete.getTile(self.tile)
 
-    def stack(self, tile: str):
-        self.tile = StackedTile(self.x, self.y, [self.tile, tile])
+    def stack(self, tile):
+        self.tile = StackedTile(self.x, self.y, [self, tile])
 
 
 class StackedTile(Tile):
+    # Readonly
     x: int
     y: int
-    tiles: List[str]
 
-    def __init__(self, x: int, y: int, tiles: List[str]):
+    tile: str
+    state: dict
+    layers: List[Tile]
+
+    def __init__(self, x: int, y: int, layers: List[Tile], state: dict = {}):
         self.x = x
         self.y = y
-        self.tiles = tiles
+        self.tile = "stacked"
+        self.layers = layers
+        self.state = state
+
+    # stacked -> tile 만 가능
+    # stacked -> stacked 불가능
+    @classmethod
+    def fromJson(cls, x: int, y: int, data: dict):
+        layers = [Tile.fromJson(x, y, tileData) for tileData in data["layers"]]
+        return cls(x, y, layers)
 
     def getTexture(self, pallete: Pallete):
         result = pygame.Surface(64, 64)
@@ -37,8 +57,8 @@ class StackedTile(Tile):
             result.blit(pallete.getTile(tile), (0, 0))
         return pallete.getTile(self.tiles)
 
-    def stack(self, tile: str):
-        self.tiles.append(tile)
+    def stack(self, tile):
+        self.layers.append(tile)
         return self
 
 
@@ -51,7 +71,7 @@ class StackedTile(Tile):
 # }
 
 
-class TownTileMap:
+class TileMap:
 
     tiles: List[List[Tile]]
 
@@ -59,10 +79,9 @@ class TownTileMap:
 
     pallete: Pallete
 
-    def __init__(self, pallete: Pallete):
-        mapData = None
+    def __init__(self, pallete: Pallete, mapFilePath: str):
         self.tiles = []
-        with open("save/town.json", "r") as file:
+        with open(mapFilePath, "r") as file:
             mapData = json.load(file)
 
         for tiles in mapData["layers"]:
@@ -81,6 +100,38 @@ class TownTileMap:
         for row in self.tiles:
             for tile in row:
                 self.screen.blit(pallete.getTile(tile.tile), (tile.x * 64, tile.y * 64))
+
+    def addTile(self, x: int, y: int, tile: str):
+        self.tiles[x][y] = Tile(x, y, tile)
+
+    def setTile(self, x: int, y: int, tile: Tile):
+        self.tiles[x][y] = tile
+
+    def setTileWithName(self, x: int, y: int, tile: str):
+        self.tiles[x][y] = Tile(x, y, tile)
+
+    def render(self, window: pygame.Surface, x, y):
+        window.blit(
+            self.screen, [-x + windowHalfWidth - 32, -y + windowHalfHeight - 32]
+        )
+
+    def getTile(self, x: int, y: int):
+        return self.tiles[x][y]
+
+    def renderTile(self, x, y, tile):
+        self.screen.blit(self.pallete.getTile(tile), (x * 64, y * 64))
+
+
+class TownTileMap(TileMap):
+
+    tiles: List[List[Tile]]
+
+    screen: pygame.Surface
+
+    pallete: Pallete
+
+    def __init__(self, pallete: Pallete):
+        super().__init__(pallete, "save/town.json")
 
     def addTile(self, x: int, y: int, tile: str):
         self.tiles[x][y] = Tile(x, y, tile)
