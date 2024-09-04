5b3aa399662d853c0f76b2ba1cdba15ef370741c
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-04 13:37:26 +0900

    scale by float

--- imageLoader.py
+++ imageLoader.py
@@ -5,7 +5,9 @@ def loadImage(sprite) -> pygame.Surface:
     return loadImageByData(sprite["imagePath"], sprite["uv"], sprite["size"])
 
 
-def loadImageByData(imagePath: str, uv: tuple, size: tuple, scale=4) -> pygame.Surface:
+def loadImageByData(
+    imagePath: str, uv: tuple, size: tuple, scale: float = 4.0
+) -> pygame.Surface:
     image = pygame.image.load(imagePath)
     rect = pygame.Rect(uv, size)
     return pygame.transform.scale(
