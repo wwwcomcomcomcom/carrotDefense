d106c1c739ab33df653d8e3e04cb8fa19905e884
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-09-04 13:37:37 +0900

    house level 2

--- object/object/house.py
+++ object/object/house.py
@@ -17,3 +17,21 @@ class House1(GameObject):
             self.sprite,
             (-x - 32 + windowHalfWidth + self.x, -y - 32 + windowHalfHeight + self.y),
         )
+
+    def nextLevel(self):
+        return House2()
+
+
+class House2(GameObject):
+
+    def __init__(self):
+        image = loadImageByData(
+            "assets\\map\\Buildings\\Purple\\PurpleKeep.png", (0, 0), (32, 32), 3 / 2
+        )
+        super().__init__(50 * 64, 50 * 64, image)
+
+    def render(self, screen: pygame.Surface, x, y):
+        screen.blit(
+            self.sprite,
+            (-x - 32 + windowHalfWidth + self.x, -y - 32 + windowHalfHeight + self.y),
+        )
